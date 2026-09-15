41688e34c0f745408f5faebd31adb002b04cb862
Edgard Severinos <djeguis@gmail.com>
2026-09-15 20:00:22 -0300

    chore: configuracion inicial del proyecto Maven

---
+++ src/main/java/cl/iplacex/examen/ExamenApplication.java
@@ -0,0 +1,15 @@
+package cl.iplacex.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Clase principal del Examen de Automatización de Pruebas.
+ */
+@SpringBootApplication
+public class ExamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenApplication.class, args);
+    }
+}
